fe1bbe5a5b2de38d6e9ef655051c325663124e43
Victor Lin <vvictor.llin@gmail.com>
2025-01-09 03:08:37 -0800

    rev

--- ps/src/snapshot.ts
+++ ps/src/snapshot.ts
@@ -1,7 +1,7 @@
-import { Battle, WeatherName } from "@pkmn/client"
-import { BoostID, BoostsTable, HitEffect, Stats, StatusName } from "@pkmn/data"
+import { Battle, Pokemon, WeatherName } from "@pkmn/client"
+import { BoostID, BoostsTable, Generation, HitEffect, Move, Stats, StatusName } from "@pkmn/data"
 
-function scale(lo: number, hi: number, v: number, neg = false) {
+function scale(v: number, lo: number, hi: number, neg = false) {
   if (neg) lo = (lo + hi) / 2
   return (v - lo) / (hi - lo)
 }
@@ -60,32 +60,32 @@ type Effect = {
   sideCondition?: string
 }
 
-function applyBoosts(effect: Effect, boosts: Partial<BoostsTable>, chance = 1) {
+function applyBoosts(effect: Effect, boosts: Partial<BoostsTable>, p = 1) {
   for (const [k, v] of Object.entries(boosts)) {
-    effect.boosts[k as BoostID] = [v, chance]
+    effect.boosts[k as BoostID] = [v, p]
   }
 }
 
 function applyHitEffect(
   effect: Effect,
-  { boosts, volatileStatus: volatile }: HitEffect,
-  chance = 1
+  { boosts, status, volatileStatus: volatile }: HitEffect,
+  p = 1
 ) {
   if (boosts) {
-    applyBoosts(effect, boosts, chance)
+    applyBoosts(effect, boosts, p)
+  }
+
+  if (status) {
+    effect.status = [status, p]
   }
 
   if (volatile) {
-    effect.volatile = [volatile, 1]
+    effect.volatile = [volatile, p]
   }
 }
 
-function extractBattle(battle: Battle) {
-  const { gens, gen, field, p1, p2 } = battle
-
-  const stats = new Stats(gens.dex)
-
-  for (const {
+function extractPokemon(gen: Generation, stats: Stats, pokemon: Pokemon) {
+  const {
     // CONSIDERED
     level,
 
@@ -213,221 +213,224 @@ function extractBattle(battle: Battle) {
 
     //derived from this.item
     hasItem
-  } of p1.team) {
-    const { baseStats } = gen.species.get(speciesForme)!
-    const { sleepTurns, toxicTurns } = statusState
+  } = pokemon
 
-    const abilityId = effectiveAbility()
+  const { baseStats } = gen.species.get(speciesForme)!
+  const { sleepTurns, toxicTurns } = statusState
 
-    return {
-      level: scale(level, 76, 100),
-      hp: scale(hp, 200, 400),
-      gender,
-      stats: Object.fromEntries(
-        (["hp", "atk", "def", "spa", "spd", "spe"] as const).map((k) => {
-          return [k, scale(stats.calc(k, baseStats.hp, 31, 85, level), 100, 300)]
-        })
-      ),
-      teraType: teraType! as string as Type,
-      terastallized: !!terastallized,
-      canTerastallize: !!canTerastallize,
-      status: status ?? null,
-      fainted,
-      boosts: Object.fromEntries(
-        Object.entries(boosts).map(([k, v]) => {
-          return [k, scale(v, -6, 6, true)]
-        })
-      ),
-      sleepTurnsLeft: status === "slp" ? [1 - sleepTurns, 3 - sleepTurns] : [0, 0],
-      toxicTurns,
-      itemId: itemId ?? null,
-      abilityId: abilityId ?? null,
-      maybeTrapped: !!maybeTrapped,
-      trapped: !!trapped,
-      types: types as string[] as Type[],
-      addedType: addedType ? (addedType as string as Type) : null,
-      switching: switching ?? null,
-      grounded: isGrounded(),
-      active: isActive(),
-      volatiles: Object.keys(volatiles).filter((v) => VOLATILES.includes(v as any)) as Volatile[],
+  const abilityId = effectiveAbility()
 
-      moves: moveSlots.map(({ id, ppUsed }) => {
-        const {
-          basePower,
-          accuracy,
-          pp,
-          category,
-          type,
-          priority,
-          target,
-          flags,
-
-          sideCondition,
-          terrain,
-          pseudoWeather,
-          weather: weatherId,
-
-          drain,
-          critRatio,
-          ignoreImmunity,
-
-          secondaries,
-          recoil,
-          self,
-          selfSwitch,
-          overrideDefensiveStat,
-          multiaccuracy,
-          multihit,
-          thawsTarget,
-          stallingMove,
-          forceSwitch,
-          selfdestruct,
-          heal,
-          overrideOffensiveStat,
-          overrideOffensivePokemon,
-          overrideDefensivePokemon,
-          hasCrashDamage,
-          sleepUsable,
-          callsMove,
-          selfBoost,
-          damage,
-          ignoreDefensive,
-          ignoreEvasion,
-          breaksProtect,
-          ignoreAbility,
-          willCrit,
-          noPPBoosts,
-
-          // NOT CONSIDERED
-
-          // covered by secondaries
-          secondary,
-
-          // only used by curse, but not very useful unless given the description
-          nonGhostTarget,
-
-          // only applies in doubles & specifically dragon darts
-          smartTarget
-        } = gen.moves.get(id)!
-
-        const effects: { self: Effect; foe: Effect } = {
-          self: {
-            boosts: {}
-          },
-          foe: {
-            boosts: {}
-          }
-        }
-
-        if (selfBoost) {
-          const { boosts } = selfBoost
-          if (boosts) {
-            applyBoosts(effects.self, boosts)
-          }
-        }
-
-        if (self) {
-          applyHitEffect(effects.self, self)
-        }
-
-        if (secondaries) {
-          for (const secondary of secondaries) {
-            const { chance, self } = secondary
-            if (!chance) continue
-            if (self) {
-              applyHitEffect(effects.self, self, chance)
-            } else {
-              applyHitEffect(effects.foe, secondary, chance)
-            }
-          }
-        }
-
-        if (boosts) {
-          switch (target) {
-            case "self":
-              applyBoosts(effects.self, boosts, 1)
-              break
-            default:
-              throw Error(target)
-          }
-        }
-
-        let weather: WeatherName | null = null
-
-        if (weatherId) {
-          weather = (
-            {
-              RainDance: "Rain",
-              sunnyday: "Sun",
-              snow: "Snow"
-            } as const
-          )[weatherId as string]!
-        }
-
-        if (sideCondition) {
-          switch (target) {
-            case "allySide":
-              effects.self.sideCondition = sideCondition
-              break
-            case "foeSide":
-              effects.foe.sideCondition = sideCondition
-              break
-            default:
-              throw Error(target)
-          }
-        }
-
-        return {
-          id,
-          ppLeft: pp - ppUsed,
-          accuracy: scale(accuracy === true ? 100 : accuracy, 0, 100),
-          category,
-
-          terrain,
-
-          // attack
-          critRatio: critRatio ?? 1,
-          basePower: scale(basePower, 0, 200),
-          damageByLevel: damage === "level",
-          statOverride: {
-            defensive: overrideDefensiveStat || overrideDefensivePokemon || null,
-            offensive: overrideOffensiveStat || overrideOffensivePokemon || null
-          },
-          multiHit: {
-            times: Array.isArray(multihit) ? multihit : [multihit ?? 0, multihit ?? 0],
-            recheckAcc: !!multiaccuracy
-          },
-          willCrit: !!willCrit,
-
-          effects: effects,
-
-          // heal % of user hp
-          heal: heal ? heal[0] / heal[1] : 0,
-
-          // steal % of foe hp
-          drain: drain ? drain[0] / drain[1] : 0,
-
-          thawsTarget: !!thawsTarget,
-          recoil: recoil ? recoil[0] / recoil[1] : 0,
-          hasCrashDamage: !!hasCrashDamage,
-          sleepUsable: !!sleepUsable,
-          selfSwitch: !!selfSwitch,
-          forceSwitch: !!forceSwitch,
-          noPPBoosts: !!noPPBoosts,
-          selfDestruct: !!selfdestruct,
-          stallingMove: !!stallingMove,
-          callsMove: !!callsMove,
-          breaksProtect: !!breaksProtect,
-          ignore: {
-            immunity: !!ignoreImmunity,
-            defensive: !!ignoreDefensive,
-            evasion: !!ignoreEvasion,
-            ability: !!ignoreAbility
-          },
-          type,
-          priority
-        }
+  return {
+    level: scale(level, 76, 100),
+    hp: scale(hp, 200, 400),
+    gender,
+    stats: Object.fromEntries(
+      (["hp", "atk", "def", "spa", "spd", "spe"] as const).map((k) => {
+        return [k, scale(stats.calc(k, baseStats.hp, 31, 85, level), 100, 300)]
       })
+    ),
+    teraType: teraType! as string as Type,
+    terastallized: !!terastallized,
+    canTerastallize: !!canTerastallize,
+    status: status ?? null,
+    fainted,
+    boosts: Object.fromEntries(
+      Object.entries(boosts).map(([k, v]) => {
+        return [k, scale(v, -6, 6, true)]
+      })
+    ),
+    sleepTurnsLeft: status === "slp" ? [1 - sleepTurns, 3 - sleepTurns] : [0, 0],
+    toxicTurns,
+    itemId: itemId ?? null,
+    abilityId: abilityId ?? null,
+    maybeTrapped: !!maybeTrapped,
+    trapped: !!trapped,
+    types: types as string[] as Type[],
+    addedType: addedType ? (addedType as string as Type) : null,
+    switching: switching ?? null,
+    grounded: isGrounded(),
+    active: isActive(),
+    volatiles: Object.keys(volatiles).filter((v) => VOLATILES.includes(v as any)) as Volatile[]
+  }
+}
+
+export function extractMove(move: Move, ppUsed = 0) {
+  const {
+    basePower,
+    accuracy,
+    pp,
+    category,
+    type,
+    priority,
+    target,
+    flags,
+
+    sideCondition,
+    terrain,
+    pseudoWeather,
+    weather: weatherId,
+
+    drain,
+    critRatio,
+    ignoreImmunity,
+
+    secondaries,
+    recoil,
+    self,
+    selfSwitch,
+    overrideDefensiveStat,
+    multiaccuracy,
+    multihit,
+    thawsTarget,
+    stallingMove,
+    forceSwitch,
+    selfdestruct,
+    heal,
+    overrideOffensiveStat,
+    overrideOffensivePokemon,
+    overrideDefensivePokemon,
+    hasCrashDamage,
+    sleepUsable,
+    callsMove,
+    selfBoost,
+    damage,
+    ignoreDefensive,
+    ignoreEvasion,
+    breaksProtect,
+    ignoreAbility,
+    willCrit,
+    noPPBoosts,
+
+    // NOT CONSIDERED
+
+    // covered by secondaries
+    secondary,
+
+    // only used by curse, but not very useful unless given the description
+    nonGhostTarget,
+
+    // only applies in doubles & specifically dragon darts
+    smartTarget
+  } = move
+
+  const effects: { self: Effect; foe: Effect } = {
+    self: {
+      boosts: {}
+    },
+    foe: {
+      boosts: {}
+    }
+  }
+
+  if (selfBoost) {
+    const { boosts } = selfBoost
+    if (boosts) {
+      applyBoosts(effects.self, boosts)
+    }
+  }
+
+  if (self) {
+    applyHitEffect(effects.self, self)
+  }
+
+  if (secondaries) {
+    for (const secondary of secondaries) {
+      const { chance, self } = secondary
+      if (!chance) continue
+      if (self) {
+        applyHitEffect(effects.self, self, chance / 100)
+      } else {
+        applyHitEffect(effects.foe, secondary, chance / 100)
+      }
+    }
+  }
+
+  switch (target) {
+    case "self":
+      applyHitEffect(effects.self, move, 1)
+      break
+    case "normal":
+      applyHitEffect(effects.foe, move, 1)
+      break
+    default:
+      break
+  }
+
+  let weather: WeatherName | null = null
+
+  if (weatherId) {
+    weather = (
+      {
+        RainDance: "Rain",
+        sunnyday: "Sun",
+        snow: "Snow"
+      } as const
+    )[weatherId as string]!
+  }
+
+  if (sideCondition) {
+    switch (target) {
+      case "allySide":
+        effects.self.sideCondition = sideCondition
+        break
+      case "foeSide":
+        effects.foe.sideCondition = sideCondition
+        break
+      default:
+        throw Error(target)
+    }
+  }
+
+  return {
+    ppLeft: pp - ppUsed,
+    accuracy: accuracy === true ? 1 : accuracy / 100,
+    category,
+    type,
+    priority,
+
+    flags: Object.keys(flags),
+    weather,
+    pseudoWeather: pseudoWeather ?? null,
+    terrain: terrain ?? null,
+
+    // attack
+    critRatio: category === "Status" ? 0 : critRatio,
+    basePower: scale(basePower, 0, 200),
+    damageByLevel: damage === "level",
+    statOverride: {
+      defensive: overrideDefensiveStat || overrideDefensivePokemon || null,
+      offensive: overrideOffensiveStat || overrideOffensivePokemon || null
+    },
+    multiHit: {
+      times: Array.isArray(multihit) ? multihit : [multihit ?? 0, multihit ?? 0],
+      recheckAcc: !!multiaccuracy
+    },
+    willCrit: !!willCrit,
+
+    effects,
+
+    // heal % of user hp
+    heal: heal ? heal[0] / heal[1] : 0,
+
+    // steal % of foe hp
+    drain: drain ? drain[0] / drain[1] : 0,
+
+    recoil: recoil ? recoil[0] / recoil[1] : 0,
+    thawsTarget: !!thawsTarget,
+    hasCrashDamage: !!hasCrashDamage,
+    sleepUsable: !!sleepUsable,
+    selfSwitch: !!selfSwitch,
+    forceSwitch: !!forceSwitch,
+    noPPBoosts: !!noPPBoosts,
+    selfDestruct: !!selfdestruct,
+    stallingMove: !!stallingMove,
+    callsMove: !!callsMove,
+    breaksProtect: !!breaksProtect,
+    ignore: {
+      immunity: !!ignoreImmunity,
+      defensive: !!ignoreDefensive,
+      evasion: !!ignoreEvasion,
+      ability: !!ignoreAbility
     }
   }
 }
---
+++ ps/src/test/snapshot.ts
@@ -0,0 +1,140 @@
+import { Generations } from "@pkmn/data"
+import { Dex } from "@pkmn/dex"
+import { extractMove } from "../snapshot.js"
+import { assert } from "console"
+
+const gen = new Generations(Dex).get(9)
+const EPS = 0.01
+
+function eq(a: number, b: number) {
+  return Math.abs(a - b) < EPS
+}
+
+{
+  const { multiHit } = extractMove(gen.moves.get("Triple Axel")!)
+  const {
+    times: [min, max],
+    recheckAcc
+  } = multiHit
+
+  assert(min === 3)
+  assert(max === 3)
+  assert(recheckAcc === true)
+}
+
+{
+  const { heal } = extractMove(gen.moves.get("Slack Off")!)
+  assert(heal === 0.5)
+}
+
+{
+  const {
+    statOverride: { offensive }
+  } = extractMove(gen.moves.get("bodypress")!)
+  assert(offensive === "def")
+}
+
+{
+  const {
+    statOverride: { defensive }
+  } = extractMove(gen.moves.get("Psyshock")!)
+  assert(defensive === "def")
+}
+
+{
+  const { thawsTarget } = extractMove(gen.moves.get("Scorching Sands")!)
+  assert(thawsTarget === true)
+}
+{
+  const { selfSwitch } = extractMove(gen.moves.get("Volt Switch")!)
+  assert(selfSwitch === true)
+}
+{
+  const { selfDestruct } = extractMove(gen.moves.get("Explosion")!)
+  assert(selfDestruct === true)
+}
+
+{
+  const { recoil } = extractMove(gen.moves.get("Flare Blitz")!)
+  assert(eq(recoil, 1 / 3), `${recoil}`)
+}
+
+{
+  const { effects } = extractMove(gen.moves.get("sludgebomb")!)
+  const [status, p] = effects.foe.status!
+  assert(status === "psn")
+  assert(p && eq(p, 0.3))
+}
+
+{
+  const { drain } = extractMove(gen.moves.get("gigadrain")!)
+  assert(eq(drain, 0.5))
+}
+
+{
+  const { effects } = extractMove(gen.moves.get("outrage")!)
+  const [volatile, p] = effects.self.volatile!
+  assert(volatile === "lockedmove", volatile)
+  assert(eq(p, 1))
+}
+
+{
+  const {
+    ignore: { evasion, defensive }
+  } = extractMove(gen.moves.get("sacredsword")!)
+  assert(defensive === true)
+  assert(evasion === true)
+}
+
+{
+  const { breaksProtect } = extractMove(gen.moves.get("hyperspacefury")!)
+  assert(breaksProtect === true)
+}
+
+{
+  const {
+    ignore: { ability }
+  } = extractMove(gen.moves.get("sunsteelstrike")!)
+  assert(ability === true)
+}
+
+{
+  const { willCrit } = extractMove(gen.moves.get("wickedblow")!)
+  assert(willCrit === true)
+}
+
+{
+  const { noPPBoosts } = extractMove(gen.moves.get("revivalblessing")!)
+  assert(noPPBoosts === true)
+}
+
+{
+  const { effects } = extractMove(gen.moves.get("toxicspikes")!)
+  assert(effects.foe.sideCondition === "toxicspikes")
+}
+
+{
+  const { effects } = extractMove(gen.moves.get("lightscreen")!)
+  assert(effects.self.sideCondition === "lightscreen")
+}
+
+{
+  const { effects } = extractMove(gen.moves.get("protect")!)
+  const [volatile, p] = effects.self.volatile!
+  assert(volatile === "protect")
+  assert(p === 1)
+}
+
+{
+  const { weather } = extractMove(gen.moves.get("raindance")!)
+  assert(weather === "Rain")
+}
+
+{
+  const { effects } = extractMove(gen.moves.get("firefang")!)
+  const { status: [status, p1] = [], volatile: [volatile, p2] = [] } = effects.foe
+  assert(status === "brn")
+  assert(eq(p1!, 0.1))
+  assert(volatile === "flinch")
+  assert(eq(p2!, 0.1))
+}
