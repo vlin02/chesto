a10d7ac37e0d8327d553cf1e5c76c65369cfa90d
Victor Lin <vvictor.llin@gmail.com>
2025-02-12 06:22:02 +0000

    rev

--- model/src/b.py
+++ model/src/b.py
@@ -1,18 +1,4 @@
-from pymongo import MongoClient
-import sys
+import torch
 
-def main():
-   client = MongoClient("mongodb://localhost:27017")
-   db = client.get_database("chesto")
-
-   i = 0
-   for _ in db.replays.find(
-       {"steps.0.sample": {"$exists": True}}, 
-       {"steps.0.sample": 1}
-   ).batch_size(1000):
-       i += 1
-       if i % 1000 == 0:
-           print(i)
-           sys.stdout.flush()
-
-main()
+cuda_available = torch.cuda.is_available()
+print(f"CUDA available: {cuda_available}")
