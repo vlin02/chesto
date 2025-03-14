d8c8f53199bdbf8f3c2b8c075bc7fd08a7b405c1
Victor Lin <vvictor.llin@gmail.com>
2025-03-14 07:16:36 +0000

    rev

--- core/src/env/server.ts
+++ core/src/env/server.ts
@@ -4,15 +4,15 @@ import { randomUUID } from "crypto"
 import { Action } from "@pkmn/sim"
 import path, { dirname } from "path"
 import { fileURLToPath } from "url"
+import { EnvUpdate } from "./env.js"
 
 const NUM_WORKERS = 60
-
 const __filename = fileURLToPath(import.meta.url)
 const WORKER_PATH = path.join(dirname(__filename), "./worker.js")
 
 type Session = {
   workerId: number
-  resolve: (update: Update) => void
+  resolve: (update: EnvUpdate) => void
 }
 
 const sessions = new Map<string, Session>()
@@ -20,7 +20,7 @@ const sessions = new Map<string, Session>()
 const workers = Array.from({ length: NUM_WORKERS }, () => new Worker(WORKER_PATH))
 
 workers.forEach((worker) => {
-  worker.on("message", ([id, update]: [string, Update]) => {
+  worker.on("message", ([id, update]: [string, EnvUpdate]) => {
     sessions.get(id)!.resolve!(update)
   })
 })
@@ -30,7 +30,7 @@ app.post("/new", async (c) => {
   const id = randomUUID()
   const workerId = Math.floor(Math.random() * NUM_WORKERS)
 
-  const update = await new Promise<Update>((resolve) => {
+  const update = await new Promise<EnvUpdate>((resolve) => {
     sessions.set(id, { workerId, resolve })
     workers[workerId].postMessage([id, { type: "start" }])
   })
@@ -45,7 +45,7 @@ app.post("/:id/step", async (c) => {
   const session = sessions.get(id)!
   const { workerId } = session
 
-  const update = await new Promise<Update>((resolve) => {
+  const update = await new Promise<EnvUpdate>((resolve) => {
     session.resolve = resolve
     workers[workerId].postMessage([id, { type: "step", actions }])
   })
